60c54176f554e2e47aa424ca08fb579baa669c15
培根必須是脆的 <baconmustbecrispy@gmail.com>
2022-12-13 16:25:11 +0800

    fix: remove unused

--- windi.config.js
+++ windi.config.js
@@ -3,6 +3,5 @@ export default {
     require('@windicss/plugin-icons'),
     require('windicss/plugin/aspect-ratio'),
     require('@windicss/plugin-scrollbar'),
-    require('@windicss/plugin-question-mark'),
   ],
 };
